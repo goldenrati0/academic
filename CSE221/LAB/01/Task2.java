ba35f29491971347b47a51e49aa379b588b42b14
tahmid-choyon <tahmid.choyon@gmail.com>
2016-09-28 03:55:55 +0600

    CSE221 LAB01

--- CSE221/LAB/01/Task2.java
+++ CSE221/LAB/01/Task2.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.io.File;
 
 public class Task2 {
@@ -10,10 +11,15 @@ public class Task2 {
 		int node = ana.nextInt();
 		int edge = ana.nextInt();
 
-		String[] adjList = new String[node];
+		ArrayList<Integer[]> arrayList = new ArrayList<Integer[]>();
 
-		for(int i=0; i<adjList.length; i++) {
-			adjList[i] = "";
+		for (int i=0; i<node; i++) {
+			
+			arrayList.add(new Integer[edge]);
+
+			Integer[] temp = arrayList.get(i);
+
+			fillArray(temp, null);
 		}
 
 		while(ana.hasNext()) {
@@ -21,11 +27,36 @@ public class Task2 {
 			int nodeIndex = ana.nextInt();
 			int edgeIndex = ana.nextInt();
 
-			adjList[nodeIndex] += "->[ " + edgeIndex + " ]";
+			Integer[] temp = arrayList.get(nodeIndex);
+
+			for (int i=0; i<temp.length; i++) {
+				
+				if(temp[i] == null) {
+					temp[i] = edgeIndex;
+					break;
+				}
+			}
 		}
 
-		for(int i=0; i<adjList.length; i++) {
-			System.out.println("[ " + i + " ]"+adjList[i]);
+		for (int i=0; i<arrayList.size(); i++) {
+			
+			Integer[] temp = arrayList.get(i);
+
+			System.out.print("["+i+"]");
+			for (Integer val : temp) {
+				
+				if(val != null)
+					System.out.print("-->["+val+"]");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void fillArray(Object[] x, Object val) {
+
+		for (int i=0; i<x.length; i++) {
+			
+			x[i] = val;
 		}
 	}
 }
