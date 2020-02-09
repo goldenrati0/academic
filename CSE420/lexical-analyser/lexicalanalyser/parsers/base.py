916299d9f934e642e8eda5651772bd5c2e88eec0
Tahmid Choyon <tahmid.choyon@gmail.com>
2020-02-09 19:55:51 +0600

    Implement start_parsing, parse in base parser

--- CSE420/lexical-analyser/lexicalanalyser/parsers/base.py
+++ CSE420/lexical-analyser/lexicalanalyser/parsers/base.py
@@ -15,10 +15,12 @@ class BaseParser(ABC):
         return re.compile(self.REGEX_STR)
 
     def parse(self, to_parse):
-        pass
+        return self._get_pattern().findall(to_parse)
 
     def start_parsing(self):
-        pass
+        while line := self._file_reader.readline():
+            parsed_data = self.parse(to_parse=line)
+            self.parsed_data.extend(parsed_data)
 
     def get_parsed_data(self, file_reader: _io.TextIOWrapper) -> List:
         self._file_reader = file_reader
