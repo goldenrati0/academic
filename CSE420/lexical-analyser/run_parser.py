847127ec6dcc85f56fc4c6e90eec3ad249ee2ad8
Tahmid Choyon <tahmid.choyon@gmail.com>
2020-02-09 19:56:55 +0600

    Change file path to relative path

--- CSE420/lexical-analyser/run_parser.py
+++ CSE420/lexical-analyser/run_parser.py
@@ -1,5 +1,5 @@
 from lexicalanalyser.parsers import start_parsing
 
 if __name__ == '__main__':
-    start_parsing('/Users/newscred/workspace/python/lexical-analyser/data/input.txt', 'id',
-                  'keyword', 'value', 'math-operator', 'logical-operator', 'other')
+    start_parsing('./data/input.txt', 'id', 'keyword', 'value', 'math-operator',
+                  'logical-operator', 'other')
